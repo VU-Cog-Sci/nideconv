7e99f8f7cf799ad3c71266fb7d7083b034595c8a
Gilles86 <Gilles.de.Hollander@gmail.com>
2018-04-18 16:13:34 +0200

    Little convenience function

--- response_fytter/utils.py
+++ response_fytter/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy as sp
 
 def get_proper_interval(interval, sample_duration):
     interval = np.array(interval)
@@ -6,3 +7,19 @@ def get_proper_interval(interval, sample_duration):
     new_interval_duration = np.floor(old_duration / sample_duration)  * sample_duration
     return np.array([interval[0], interval[0] + new_interval_duration])
 
+def resample_and_zscore(s, old_samplerate=1000, target_samplerate=20):
+    
+    if old_samplerate / target_samplerate < 13:
+        downsample_factor = int(old_samplerate / target_sample_rate)
+        s = sp.signal.decimate(s.values.ravel(), downsample_factor)
+        new_samplerate = old_samplerate / downsample_factor
+    else:
+        downsample_factor = int(np.sqrt(old_samplerate / target_samplerate))
+        s = sp.signal.decimate(s.values.ravel(), downsample_factor)
+        s = sp.signal.decimate(s, downsample_factor)
+        
+        new_samplerate = old_samplerate / downsample_factor**2
+        
+    s -= s.mean()
+    s /= s.std()
+    return s, new_samplerate
