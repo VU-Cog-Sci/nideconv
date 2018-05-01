9eeeb83823c0613cea09a178154b29014857c7b5
Gilles de Hollander <gilles.de.hollander@gmail.com>
2018-05-01 14:28:49 +0200

    Various plotting improvements

--- response_fytter/group_analysis.py
+++ response_fytter/group_analysis.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import warnings
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 class GroupResponseFytter(object):
 
@@ -149,7 +150,7 @@ class GroupResponseFytter(object):
 
         if melt:
             return tc.reset_index().melt(id_vars=tc.index.names,
-                                         var_name='signal')
+                                         var_name='roi')
         else:
             return tc
 
@@ -184,13 +185,17 @@ class GroupResponseFytter(object):
                 raise NotImplementedError("kind should be 'mean', 't', or 'z'")
 
     def plot_groupwise_timecourses(self,
-                                   plots='signal',
+                                   plots='roi',
                                    col='covariate',
                                    row=None,
                                    col_wrap=None,
                                    hue='event type',
                                    max_n_plots=40,
                                    oversample=None,
+                                   extra_axes=True,
+                                   sharex=True,
+                                   sharey=False,
+                                   aspect=1.5,
                                    *args,
                                    **kwargs):
 
@@ -201,11 +206,18 @@ class GroupResponseFytter(object):
         if len(tc[plots].unique()) > max_n_plots:
             raise Exception('Splitting over %s would mean more than %d plots!')
 
-        for idx, label in tc.groupby(plots):
-            fac = sns.FacetGrid(tc.reset_index(),
+        facs = []
+
+        multiple_plots = len(tc[plots].unique()) > 1
+
+        for idx, plot_df in tc.groupby(plots):
+            fac = sns.FacetGrid(plot_df,
                                 col_wrap=col_wrap,
                                 col=col,
-                                row=row)
+                                row=row,
+                                sharex=sharex,
+                                sharey=sharey,
+                                aspect=aspect)
 
             fac.map_dataframe(sns.tsplot,
                               time='time',
@@ -216,7 +228,17 @@ class GroupResponseFytter(object):
                               *args,
                               **kwargs)
 
-        return fac
+            if extra_axes:
+                fac.map(plt.axhline, y=0, c='k', ls='--')
+                fac.map(plt.axvline, x=0, c='k', ls='--')
+
+            fac.fig.subplots_adjust(top=0.8)
+            fac.fig.suptitle(idx, fontsize=16)
+
+            facs.append(fac)
+
+        return facs
+
 
 def _make_time_column(d, sample_rate):
     return pd.DataFrame(np.arange(0, len(d) * 1./sample_rate, 1./sample_rate), index=d.index)
