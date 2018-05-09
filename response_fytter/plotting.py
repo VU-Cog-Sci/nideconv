9760d3b035bd140fb1ef04734a2a45e72ef7df55
Gilles de Hollander <gilles.de.hollander@gmail.com>
2018-05-09 16:20:11 +0200

    Allow for plotting only one figure

--- response_fytter/plotting.py
+++ response_fytter/plotting.py
@@ -18,15 +18,11 @@ def plot_timecourses(tc,
                      **kwargs):
 
     
-    if len(tc[plots].unique()) > max_n_plots:
-        raise Exception('Splitting over %s would mean more than %d plots!')
-
     facs = []
+    idxs = []
 
-    multiple_plots = len(tc[plots].unique()) > 1
-
-    for idx, plot_df in tc.groupby(plots):
-        fac = sns.FacetGrid(plot_df,
+    if plots is None:
+        fac = sns.FacetGrid(tc,
                             col_wrap=col_wrap,
                             col=col,
                             row=row,
@@ -43,6 +39,35 @@ def plot_timecourses(tc,
                           *args,
                           **kwargs)
 
+        facs.append(fac)
+        idxs.append('')
+
+    else:
+        if len(tc[plots].unique()) > max_n_plots:
+            raise Exception('Splitting over %s would mean more than %d plots!')
+
+
+        for idx, plot_df in tc.groupby(plots):
+            fac = sns.FacetGrid(plot_df,
+                                col_wrap=col_wrap,
+                                col=col,
+                                row=row,
+                                sharex=sharex,
+                                sharey=sharey,
+                                aspect=aspect)
+
+            fac.map_dataframe(sns.tsplot,
+                              time='time',
+                              unit='subj_idx',
+                              condition=hue,
+                              value='value',
+                              color=sns.color_palette(),
+                              *args,
+                              **kwargs)
+            facs.append(fac)
+            idxs.append(idx)
+
+    for fac, idx in zip(facs, idxs):
         if extra_axes:
             fac.map(plt.axhline, y=0, c='k', ls='--')
             fac.map(plt.axvline, x=0, c='k', ls='--')
@@ -50,6 +75,5 @@ def plot_timecourses(tc,
         fac.fig.subplots_adjust(top=0.8)
         fac.fig.suptitle(idx, fontsize=16)
 
-        facs.append(fac)
 
     return facs
