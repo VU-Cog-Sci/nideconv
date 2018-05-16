2a1456aab8758f8eed9a90cc676bfab5adeabce8
Gilles de Hollander <gilles.de.hollander@gmail.com>
2018-05-16 14:23:11 +0200

    First version of GroupResponseFytter

--- response_fytter/nifti.py
+++ response_fytter/nifti.py
@@ -14,7 +14,6 @@ class NiftiResponseFytter(ResponseFytter):
                  mask=None,
                  oversample_design_matrix=20,
                  add_intercept=True,
-                 threshold=0,
                  detrend=False,
                  standardize=False,
                  confounds_for_extraction=None,
@@ -84,10 +83,11 @@ class NiftiResponseFytter(ResponseFytter):
 
         else:
             tc_df = []
-            for (event_type, covariate), tc in timecourses.groupby(level=['event type', 'covariate']):
+            for (event_type, covariate, time), tc in timecourses.groupby(level=['event type', 'covariate', 'time']):
+                #timepoints = tc.index.get_level_values('time')
                 tc_nii = self._inverse_transform(tc)
-                tc = pd.DataFrame([tc_nii], index=pd.MultiIndex.from_tuples([(event_type, covariate)],
-                                                                         names=['event type', 'covariate']),
+                tc = pd.DataFrame([tc_nii], index=pd.MultiIndex.from_tuples([(event_type, covariate, time)],
+                                                                         names=['event type', 'covariate', 'time']),
                                   columns=['nii'])
                 tc_df.append(tc)
 
@@ -100,4 +100,166 @@ class NiftiResponseFytter(ResponseFytter):
                            data):
 
         return self.masker.inverse_transform(data)
+
+
+class GroupNiftiResponseFytter(object):
+
+
+    def __init__(self,
+                 oversample_design_matrix=20,
+                 detrend=False,
+                 standardize=False,
+                 memory=None,
+                 add_intercept=True,
+                 **kwargs):
+        
+        self.oversample_design_matrix = oversample_design_matrix
+        self.add_intercept = add_intercept
+
+        self.detrend = detrend
+        self.standardize = standardize
+        self.memory = memory
+
+        self.events = []
+        self.images = []
+
+
+
+    
+    def add_event(self,
+                 event=None,
+                 basis_set='fir', 
+                 interval=[0,10], 
+                 n_regressors=None, 
+                 covariates=None,
+                 add_intercept=True,
+                 **kwargs):
+
+
+        event_kws = {'event':event,
+                     'basis_set':basis_set,
+                     'interval':interval,
+                     'n_regressors':n_regressors,
+                     'covariates':covariates,
+                     'add_intercept':add_intercept
+                     }
+        event_kws.update(kwargs)
+
+        self.events.append(event_kws)
+
+    
+    def add_image(self,
+                  func_img,
+                  behavior,
+                  sample_rate,
+                  subj_idx,
+                  run=None,
+                  mask=None,
+                  confounds=None):
+       
+        image_kws = {'func_img':func_img,
+                     'behavior':behavior,
+                     'subj_idx':subj_idx,
+                     'run':run,
+                     'mask':mask,
+                     'confounds':confounds,
+                     'sample_rate':sample_rate}
+
+        self.images.append(image_kws)
+
+
+    def fit(self):
+
+        self.timecourses = pd.DataFrame()
+
+        self.maskers = pd.DataFrame([],
+                                    index=pd.MultiIndex.from_tuples([], names=['subj_idx', 'run']),
+                                    columns=['masker'])
+
+        for image in self.images:
+
+            logging.info('Fitting subject {subj_idx}, run {run}'.format(**image))
+            
+            fitter = NiftiResponseFytter(image['func_img'],
+                                         image['sample_rate'],
+                                         image['mask'],
+                                         self.oversample_design_matrix,
+                                         self.add_intercept,
+                                         self.detrend,
+                                         self.standardize,
+                                         memory=self.memory)
+
+            self.maskers.loc[(image['subj_idx'], image['run']), 
+                             'masker'] = fitter.masker
+
+            for event in self.events:
+
+                behavior = image['behavior']
+                behavior = behavior[behavior.trial_type == event['event']] 
+
+                if event['covariates'] is None:
+                    covariate_matrix = None
+                else:
+                    covariate_matrix = behavior[covariates]
+
+                    if add_intercept:
+                        intercept_matrix = pd.DataFrame(np.ones((len(covariate_matrix), 1)),
+                                                        columns=['intercept'],
+                                                        index=covariate_matrix.index)
+                        covariate_matrix = pd.concat((intercept_matrix, covariate_matrix), 1)
+                
+                if 'duration' in behavior and np.isfinite(behavior['duration']).all():
+                    durations = behavior['duration']
+                else:
+                    durations = None
+
+                fitter.add_event(event['event'],
+                                 behavior.onset,
+                                 event['basis_set'],
+                                 event['interval'],
+                                 event['n_regressors'],
+                                 durations,
+                                 covariate_matrix)
+
+                if image['confounds'] is not None:
+                    fitter.add_confounds('confounds', image['confounds'])
+        
+            logging.info(fitter.X.columns.tolist())
+            fitter.regress()
+
+
+            tc = fitter.get_timecourses()
+            
+            del fitter
+
+            tc['subj_idx'] = image['subj_idx']
+            tc['run'] = image['run']
+
+            tc.set_index(['subj_idx', 'run'])
+
+            self.timecourses = pd.concat((self.timecourses, tc), 0)
+            
+        self.timecourses = self.timecourses.reset_index().set_index(['subj_idx', 'run', 'event type', 'covariate', 'time'])
+
+
+    def get_timecourses(self, mask=None, names=None):
+
+        if mask is None:
+            return self.timecourses
+
+        if type(mask) is not list:
+            mask = [mask]
+
+
+        if names:
+            names = pd.Index(names, name='roi')
+
+        masker = input_data.NiftiMapsMasker(mask)
+
+        masker.fit()
+
+        timecourses = self.timecourses.nii.apply(lambda d: pd.Series(masker.transform(d).squeeze(), 
+                                                                index=names))
+
+        return timecourses
 
