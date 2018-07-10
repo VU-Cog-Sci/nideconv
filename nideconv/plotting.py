f3b69fae16fa735cde56301c913f77323ce3ba85
Gilles de Hollander <gilles.de.hollander@gmail.com>
2018-07-10 15:47:44 +0200

    Fix plotting

--- nideconv/plotting.py
+++ nideconv/plotting.py
@@ -52,6 +52,11 @@ def plot_timecourses(tc,
         fac.fig.subplots_adjust(top=0.8)
         fac.fig.suptitle(idx, fontsize=16)
 
+
+        if len(fac.col_names) == 1:
+            fac.set_titles('')
+
         facs.append(fac)
+
 
     return facs
