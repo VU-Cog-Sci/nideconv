5a1d6ba2fe9437b10663f49b3ff9960c1623c6a1
Gilles de Hollander <gilles.de.hollander@gmail.com>
2018-07-10 15:48:17 +0200

    Small bugfix

--- nideconv/response_fitter.py
+++ nideconv/response_fitter.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 import scipy as sp
 from .plotting import plot_timecourses
+from nilearn import input_data, image
+from nilearn._utils import load_niimg
 from .utils import get_time_to_peak_from_timecourse
 
 class ResponseFitter(object):
@@ -265,7 +267,7 @@ class ResponseFitter(object):
                                   oversample=oversample)
         tc['subj_idx'] = 'dummy'
 
-        plot_timecourses(tc, *args, **kwargs)
+        return plot_timecourses(tc, *args, **kwargs)
 
     def get_rsq(self):
         """
