946ca643d7e6863aaa705610757f662ef11799a5
gjheij <j.heij@spinozacentre.nl>
2025-03-02 17:19:26 +0100

    make nifti class sort of working (#30)

--- nideconv/nifti.py
+++ nideconv/nifti.py
@@ -1,4 +1,5 @@
 from .response_fitter import ResponseFitter
+from .utils import roi
 from nilearn._utils import load_niimg
 from nilearn import maskers, image
 import pandas as pd
@@ -9,17 +10,21 @@ from .utils import get_time_to_peak_from_timecourse
 
 class NiftiResponseFitter(ResponseFitter):
 
-    def __init__(self,
-                 func_img,
-                 sample_rate,
-                 mask=None,
-                 oversample_design_matrix=20,
-                 add_intercept=True,
-                 detrend=False,
-                 standardize=False,
-                 confounds_for_extraction=None,
-                 memory=None,
-                 **kwargs):
+    def __init__(
+        self,
+        func_img,
+        sample_rate,
+        mask=None,
+        atlas=None,
+        oversample_design_matrix=20,
+        add_intercept=True,
+        detrend=False,
+        standardize=False,
+        confounds_for_extraction=None,
+        memory=None,
+        roi_kws={},
+        **kwargs
+        ):
 
 
         if isinstance(confounds_for_extraction, pd.DataFrame):
@@ -27,31 +32,43 @@ class NiftiResponseFitter(ResponseFitter):
 
         self.confounds = confounds_for_extraction
 
-        if isinstance(mask, maskers.NiftiMasker):
-            self.masker = mask
+        if atlas is not None:
+
+            input_signal = roi.extract_timecourse_from_nii(
+                atlas,
+                func_img,
+                t_r=1/sample_rate,
+                confounds=self.confounds,
+                **roi_kws
+            )
         else:
-
-            if mask is None:
-                logging.warn('No mask has been given. Nilearn will automatically try to'\
-                              'make one')
+            if isinstance(mask, maskers.NiftiMasker):
+                self.masker = mask
             else:
-                mask = load_niimg(mask)
-            self.masker = maskers.NiftiMasker(mask,
-                                                 detrend=detrend,
-                                                 standardize=standardize,
-                                                 memory=memory)
 
+                if mask is None:
+                    logging.warn('No mask has been given. Nilearn will automatically try to make one')
+                else:
+                    mask = load_niimg(mask)
 
-        input_signal = self.masker.fit_transform(func_img,
-                                                 confounds=confounds_for_extraction)
+                self.masker = maskers.NiftiMasker(
+                    mask_img=mask,
+                    detrend=detrend,
+                    standardize=standardize,
+                    memory=memory
+                )
+
+            input_signal = self.masker.fit_transform(func_img, confounds=confounds_for_extraction)
+
         self.n_voxels = input_signal.shape[1]
 
-        super(NiftiResponseFitter, self).__init__(input_signal=input_signal,
-                                           sample_rate=sample_rate,
-                                           oversample_design_matrix=oversample_design_matrix,
-                                           add_intercept=add_intercept,
-                                           **kwargs)
-
+        super().__init__(
+            input_signal=input_signal,
+            sample_rate=sample_rate,
+            oversample_design_matrix=oversample_design_matrix,
+            add_intercept=add_intercept,
+            **kwargs
+        )
 
     
     def ridge_regress(self, *args, **kwargs):
@@ -62,20 +79,24 @@ class NiftiResponseFitter(ResponseFitter):
         prediction = super(NiftiResponseFitter, self).predict_from_design_matrix(X)
         return self._inverse_transform(prediction)
 
-    def get_timecourses(self, 
-                        oversample=None,
-                        average_over_mask=False,
-                        transform_to_niftis=True,
-                        **kwargs
-                        ):
+    def get_timecourses(
+        self, 
+        oversample=None,
+        average_over_mask=False,
+        transform_to_niftis=True,
+        **kwargs
+        ):
 
         if len(self.events) is 0:
             raise Exception("No events were added")
 
-        timecourses = super(NiftiResponseFitter, self).get_timecourses(oversample=oversample,
-                                                                       melt=False,
-                                                                       **kwargs)
-        if transform_to_niftis:
+        timecourses = super().get_timecourses(
+            oversample=oversample,
+            melt=False,
+            **kwargs
+        )
+
+        if transform_to_niftis and hasattr(self, "masker"):
             if average_over_mask:
                 
                 average_over_mask = load_niimg(average_over_mask)
@@ -93,27 +114,29 @@ class NiftiResponseFitter(ResponseFitter):
                 for (event_type, covariate, time), tc in timecourses.groupby(level=['event type', 'covariate', 'time']):
                     #timepoints = tc.index.get_level_values('time')
                     tc_nii = self._inverse_transform(tc)
-                    tc = pd.DataFrame([tc_nii], index=pd.MultiIndex.from_tuples([(event_type, covariate, time)],
-                                                                             names=['event type', 'covariate', 'time']),
-                                      columns=['nii'])
+                    tc = pd.DataFrame(
+                        [tc_nii],
+                        index=pd.MultiIndex.from_tuples(
+                            [(event_type, covariate, time)],
+                            names=['event type', 'covariate', 'time']
+                        ),
+                        columns=['nii']
+                    )
                     tc_df.append(tc)
 
                 return pd.concat(tc_df)
         else:
             return timecourses
 
-                                       
-
-
-    def _inverse_transform(self, 
-                           data):
-
+    def _inverse_transform(self, data):
         return self.masker.inverse_transform(data)
 
     def get_residuals(self):
-        residuals = image.math_img('data - prediction',
-                                   data=self._inverse_transform(self.input_signal),
-                                   prediction=self.predict_from_design_matrix())
+        residuals = image.math_img(
+            'data - prediction',
+            data=self._inverse_transform(self.input_signal),
+            prediction=self.predict_from_design_matrix()
+        )
 
         return residuals
 
@@ -125,11 +148,11 @@ class NiftiResponseFitter(ResponseFitter):
 
         return self._inverse_transform(super().get_rsq())
 
-    def get_time_to_peak(self,
-                         oversample=None,
-                         negative_peak=False,
-                         include_prominence=False):
-
+    def get_time_to_peak(
+        self,
+        oversample=None,
+        negative_peak=False,
+        include_prominence=False):
         
         if oversample is None:
             oversample = self.oversample_design_matrix
@@ -139,26 +162,31 @@ class NiftiResponseFitter(ResponseFitter):
         else:
             ix = ['time peak']
 
-        peaks = self.get_timecourses(oversample=oversample,
-                                    transform_to_niftis=False)\
-                   .groupby(['event type', 'covariate'])\
-                   .apply(get_time_to_peak_from_timecourse,
-                          negative_peak=negative_peak)
+        peaks = self.get_timecourses(
+            oversample=oversample,
+            transform_to_niftis=False
+            ).groupby(['event type', 'covariate']).apply(
+                get_time_to_peak_from_timecourse,
+                negative_peak=negative_peak
+            )
         
-        return peaks.apply(lambda d: self._inverse_transform(d), 1)\
-            .to_frame('nii').loc[(slice(None), slice(None), ix), :]
+        return peaks.apply(
+            lambda d: self._inverse_transform(d), 1
+            ).to_frame('nii').loc[(slice(None), slice(None), ix), :]
 
 
 class GroupNiftiResponseFitter(object):
 
 
-    def __init__(self,
-                 oversample_design_matrix=20,
-                 detrend=False,
-                 standardize=False,
-                 memory=None,
-                 add_intercept=True,
-                 **kwargs):
+    def __init__(
+        self,
+        oversample_design_matrix=20,
+        detrend=False,
+        standardize=False,
+        memory=None,
+        add_intercept=True,
+        **kwargs
+        ):
         
         self.oversample_design_matrix = oversample_design_matrix
         self.add_intercept = add_intercept
@@ -169,72 +197,89 @@ class GroupNiftiResponseFitter(object):
 
         self.events = []
         self.images = []
-
-
-
     
-    def add_event(self,
-                 event=None,
-                 basis_set='fir', 
-                 interval=[0,10], 
-                 n_regressors=None, 
-                 covariates=None,
-                 add_intercept=True,
-                 **kwargs):
+    def add_event(
+        self,
+        event=None,
+        basis_set='fir', 
+        interval=[0,10], 
+        n_regressors=None, 
+        covariates=None,
+        add_intercept=True,
+        **kwargs):
 
 
-        event_kws = {'event':event,
-                     'basis_set':basis_set,
-                     'interval':interval,
-                     'n_regressors':n_regressors,
-                     'covariates':covariates,
-                     'add_intercept':add_intercept
-                     }
+        event_kws = {
+            'event':event,
+            'basis_set':basis_set,
+            'interval':interval,
+            'n_regressors':n_regressors,
+            'covariates':covariates,
+            'add_intercept':add_intercept
+        }
+
         event_kws.update(kwargs)
 
         self.events.append(event_kws)
 
     
-    def add_image(self,
-                  func_img,
-                  behavior,
-                  sample_rate,
-                  subj_idx,
-                  run=None,
-                  mask=None,
-                  confounds=None):
+    def add_image(
+        self,
+        func_img,
+        behavior,
+        sample_rate,
+        subject,
+        run=None,
+        mask=None,
+        atlas=None,
+        confounds=None):
        
-        image_kws = {'func_img':func_img,
-                     'behavior':behavior,
-                     'subj_idx':subj_idx,
-                     'run':run,
-                     'mask':mask,
-                     'confounds':confounds,
-                     'sample_rate':sample_rate}
+        image_kws = {
+            'func_img':func_img,
+            'behavior':behavior,
+            'subject':subject,
+            'run':run,
+            'mask':mask,
+            'atlas': atlas,
+            'confounds':confounds,
+            'sample_rate':sample_rate
+        }
 
         self.images.append(image_kws)
 
 
-    def fit(self, keep_timeseries=False):
+    def fit(
+        self,
+        store_residuals=False,
+        type='ols',
+        cv=20,
+        alphas=None,
+        keep_timeseries=False
+        ):
 
         self.timecourses = pd.DataFrame()
 
-        self.fitters = pd.DataFrame([],
-                                    index=pd.MultiIndex.from_tuples([], names=['subj_idx', 'run']),
-                                    columns=['fitter'])
+        self.fitters = pd.DataFrame(
+            [],
+            index=pd.MultiIndex.from_tuples([], names=['subject', 'run']),
+            columns=['fitter']
+        )
 
         for image in self.images:
 
-            logging.info('Fitting subject {subj_idx}, run {run}'.format(**image))
+            logging.info('Fitting subject {subject}, run {run}'.format(**image))
             
-            fitter = NiftiResponseFitter(image['func_img'],
-                                         image['sample_rate'],
-                                         image['mask'],
-                                         self.oversample_design_matrix,
-                                         self.add_intercept,
-                                         self.detrend,
-                                         self.standardize,
-                                         memory=self.memory)
+            fitter = NiftiResponseFitter(
+                image['func_img'],
+                sample_rate=image['sample_rate'],
+                mask=image['mask'],
+                atlas=image['atlas'],
+                oversample_design_matrix=self.oversample_design_matrix,
+                add_intercept=self.add_intercept,
+                detrend=self.detrend,
+                standardize=self.standardize,
+                memory=self.memory
+            )
 
 
             if image['confounds'] is not None:
@@ -243,7 +288,7 @@ class GroupNiftiResponseFitter(object):
             for event in self.events:
 
                 behavior = image['behavior']
-                behavior = behavior[behavior.trial_type == event['event']] 
+                behavior = behavior[behavior.event_type == event['event']] 
 
                 if event['covariates'] is None:
                     covariate_matrix = None
@@ -251,9 +296,11 @@ class GroupNiftiResponseFitter(object):
                     covariate_matrix = behavior[event['covariates']]
 
                     if event['add_intercept']:
-                        intercept_matrix = pd.DataFrame(np.ones((len(covariate_matrix), 1)),
-                                                        columns=['intercept'],
-                                                        index=covariate_matrix.index)
+                        intercept_matrix = pd.DataFrame(
+                            np.ones((len(covariate_matrix), 1)),
+                            columns=['intercept'],
+                            index=covariate_matrix.index
+                        )
                         covariate_matrix = pd.concat((intercept_matrix, covariate_matrix), 1)
                 
                 if 'duration' in behavior and np.isfinite(behavior['duration']).all():
@@ -261,37 +308,48 @@ class GroupNiftiResponseFitter(object):
                 else:
                     durations = None
 
-                fitter.add_event(event['event'],
-                                 behavior.onset,
-                                 event['basis_set'],
-                                 event['interval'],
-                                 event['n_regressors'],
-                                 durations,
-                                 covariate_matrix)
+                fitter.add_event(
+                    event['event'],
+                    behavior.onset,
+                    event['basis_set'],
+                    event['interval'],
+                    event['n_regressors'],
+                    durations,
+                    covariate_matrix
+                )
         
-            fitter.regress()
+            fitter.fit(
+                type=type,
+                cv=cv,
+                alphas=alphas,
+                store_residuals=store_residuals
+            )
 
             tc = fitter.get_timecourses()
 
             if not keep_timeseries:
                 tc.input_signal = None
 
-            self.fitters.loc[(image['subj_idx'], image['run']), 'fitter'] = fitter
+            self.fitters.loc[(image['subject'], image['run']), 'fitter'] = fitter
 
-            tc['subj_idx'] = image['subj_idx']
+            tc['subject'] = image['subject']
             tc['run'] = image['run']
 
-            tc.set_index(['subj_idx', 'run'])
+            tc.set_index(['subject', 'run'])
 
-            self.timecourses = pd.concat((self.timecourses, tc), 0)
+            self.timecourses = pd.concat((self.timecourses, tc), axis=0)
             
-        self.timecourses = self.timecourses.reset_index().set_index(['subj_idx', 'run', 'event type', 'covariate', 'time'])
+        self.timecourses = self.timecourses.reset_index().set_index(['subject', 'run', 'event type', 'covariate', 'time'])
 
 
-    def get_timecourses(self, mask=None, names=None,
-                        event_types=None,
-                        covariates=None,
-                        resampling_target=None):
+    def get_timecourses(
+        self,
+        mask=None,
+        names=None,
+        event_types=None,
+        covariates=None,
+        resampling_target=None
+        ):
 
         if mask is None:
             return self.timecourses
@@ -309,7 +367,7 @@ class GroupNiftiResponseFitter(object):
         if names:
             names = pd.Index(names, name='roi')
 
-        masker = maskers.NiftiMapsMasker(mask, resampling_target=resampling_target)
+        masker = maskers.NiftiMasker(mask_img=mask, resampling_target=resampling_target)
 
         masker.fit()
 
