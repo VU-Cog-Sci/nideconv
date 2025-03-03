8f390ad2c1ef8fa0d2eec4457652a02867b991bc
gjheij <j.heij@spinozacentre.nl>
2025-03-03 11:10:11 +0100

    redundant import

--- nideconv/utils/__init__.py
+++ nideconv/utils/__init__.py
@@ -1,11 +1,13 @@
-from .tools import (get_proper_interval,
-                    resample_and_zscore,
-                    convolve_with_function,
-                    gamma,
-                    double_gamma_with_d,
-                    double_gamma_with_d_time_derivative,
-                    get_time_to_peak_from_timecourse,
-                    get_ss)
+from .tools import (
+    get_proper_interval,
+    resample_and_zscore,
+    convolve_with_function,
+    gamma,
+    double_gamma_with_d,
+    double_gamma_with_d_time_derivative,
+    double_gamma_with_d_time_derivative_dispersion, 
+    get_time_to_peak_from_timecourse,
+    get_ss)
 
 
 from .roi import extract_timecourse_from_nii
