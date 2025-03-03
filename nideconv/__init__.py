aa19796e2158f682f1676b0a3e20d146beb46a92
gjheij <j.heij@spinozacentre.nl>
2025-03-03 17:02:07 +0100

    read dynamic version

--- nideconv/__init__.py
+++ nideconv/__init__.py
@@ -1,3 +1,6 @@
-from .version import __version__
+from importlib.metadata import version
+
+__version__ = version("nideconv")
+
 from .response_fitter import ResponseFitter
 from .group_analysis import GroupResponseFitter
